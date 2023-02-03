33ee424e2f4a2e671e28911477486de899dfaeae
oelin <42823429+oelin@users.noreply.github.com>
2023-02-03 12:41:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-export default function(values, min = 0, max = 10) {
+export default function(values, min = null, max = null) {
 
         if (typeof values === 'number') {
                 values = [values, ] // Convert to array if just given a single value.
         }
 
         return !values // Returns true if no values are outside the interval.
-                .filter(value => (value < min) || (value > max))
+                .filter(value => ((min != null) && (value < min)) || ((max != null) && (value > max)))
                 .length
 }
